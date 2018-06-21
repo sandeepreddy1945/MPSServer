85791a8d3e42e92ddbf746ccd0a851d37cf97833
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2018-06-21 15:45:15 +0530

    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

--- src/main/java/com/mps/app/config/SwaggerConfig.java
+++ src/main/java/com/mps/app/config/SwaggerConfig.java
@@ -1,26 +1,28 @@
 package com.mps.app.config;
 
-import java.util.ArrayList;
+import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 import io.swagger.annotations.SwaggerDefinition;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
-import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.BasicAuth;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.GrantType;
+import springfox.documentation.service.OAuth;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.SecurityScheme;
-import springfox.documentation.service.TokenEndpoint;
-import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -42,25 +44,21 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		/*
-		 * List<SecurityScheme> schemes = new ArrayList<>(); // new
-		 * ApiKeyAuthDefinition("apiKey", In.HEADER) // new ApiKey("Authorization",
-		 * "Authorization", "header") schemes.add(new BasicAuth("base"));
-		 * 
-		 * return new Docket(DocumentationType.SWAGGER_2) .apiInfo(new
-		 * ApiInfoBuilder().description("A Spring Application For MPS Services")
-		 * .license("This is Strictly Prohibited For Use by Third Parties.")
-		 * .title("A Small Application for holding MPS Services").version("1.0")
-		 * .contact(new Contact("Sandeep Reddy", "", "b.sandeepreddy209@gmail.com")) no
-		 * format .licenseUrl("") // TODO needs to be updated. .termsOfServiceUrl("") //
-		 * TODO needs to be updated. .build())
-		 * .securitySchemes(schemes).select().apis(RequestHandlerSelectors.any()).paths(
-		 * PathSelectors.any()) .build();
-		 */
 
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build().securitySchemes(Arrays.asList(securityScheme()))
-				.securityContexts(Arrays.asList(securityContext()));
+				.paths(PathSelectors.any()).build()
+				.securitySchemes(Arrays.asList(securitySchema(), apiKey(), apiCookieKey()))
+				.securityContexts(Arrays.asList(securityContext())).apiInfo(apiInfo());
+	}
+
+	@Bean
+	public SecurityScheme apiKey() {
+		return new ApiKey(HttpHeaders.AUTHORIZATION, "apiKey", "header");
+	}
+
+	@Bean
+	public SecurityScheme apiCookieKey() {
+		return new ApiKey(HttpHeaders.COOKIE, "apiKey", "cookie");
 	}
 
 	@Bean
@@ -69,28 +67,43 @@ public class SwaggerConfig {
 				.useBasicAuthenticationWithAccessCodeGrant(true).build();
 	}
 
-	private SecurityScheme securityScheme() {
-		GrantType grantType = new AuthorizationCodeGrantBuilder()
-				.tokenEndpoint(new TokenEndpoint(AUTH_SERVER + "/token", "oauthtoken"))
-				.tokenRequestEndpoint(new TokenRequestEndpoint(AUTH_SERVER + "/token", CLIENT_ID, CLIENT_ID))
-				.build();
+	private OAuth securitySchema() {
 
-		SecurityScheme oauth = new OAuthBuilder().name("spring_oauth").grantTypes(Arrays.asList(grantType))
-				.scopes(Arrays.asList(scopes())).build();
-		return oauth;
+		List<AuthorizationScope> authorizationScopeList = newArrayList();
+		authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+		authorizationScopeList.add(new AuthorizationScope("write", "access all"));
+		authorizationScopeList.add(new AuthorizationScope("admin", "trust all"));
+
+		List<GrantType> grantTypes = newArrayList();
+		GrantType passwordCredentialsGrant = new ResourceOwnerPasswordCredentialsGrant(AUTH_SERVER + "/token");
+		grantTypes.add(passwordCredentialsGrant);
+
+		return new OAuth("oauth2", authorizationScopeList, grantTypes);
 	}
 
 	private SecurityContext securityContext() {
-		return SecurityContext.builder()
-				.securityReferences(Arrays.asList(new SecurityReference("spring_oauth", scopes())))
-				.forPaths(PathSelectors.regex("/foos.*")).build();
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
 	}
 
-	private AuthorizationScope[] scopes() {
-		AuthorizationScope[] scopes = { new AuthorizationScope("read", "for read operations"),
-				new AuthorizationScope("write", "for write operations"),
-				new AuthorizationScope("admin", "Admin Operations") };
-		return scopes;
+	private List<SecurityReference> defaultAuth() {
+
+		final AuthorizationScope[] authorizationScopes = { new AuthorizationScope("read", "read all"),
+				new AuthorizationScope("write", "write all"), new AuthorizationScope("admin", "trust all") };
+
+		return Collections.singletonList(new SecurityReference("oauth2", authorizationScopes));
+	}
+
+	/**
+	 *
+	 * @return ApiInf
+	 */
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("MPS Authentication API")
+				.description("Third Party Source to Authenticate MPS Resources")
+				.termsOfServiceUrl("https://www.example.com/api")
+				.contact(new Contact("Developers", "Yet To Build This URL", "")).license("Thirdparty Usgae Tool")
+				.licenseUrl("\"https://www.apache.org/licenses/LICENSE-2.0").version("1.0.0").build();
+
 	}
 
 }
